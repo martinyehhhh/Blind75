62a87bbeb4a4f075f9538958fb65c3809de61e85
Martin Yeh <yamao8482@gmail.com>
2022-11-11 16:56:11 +0800

    finish Longest Palindromic Substring

--- Longest Palindromic Substring.py
+++ Longest Palindromic Substring.py
@@ -25,4 +25,17 @@ s consist of only digits and English letters.
 """
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        
+        n = len(s)
+        def getLen(l,r):
+            while l >= 0 and r < n and s[l] == s[r]:
+                l -= 1
+                r += 1
+            return r - l - 1
+        start = 0
+        maxLen = 0
+        for i in range(n):
+            currentLen = max(getLen(i,i),getLen(i,i+1))
+            if currentLen > maxLen:
+                maxLen = currentLen
+                start =  i - (currentLen - 1) // 2
+        return s[start:start+maxLen]
